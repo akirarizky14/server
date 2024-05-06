b99e6ec7d996d4d2c4c2f30522c5489d410369b0
akirarizky14 <akirarizkyyy@gmail.com>
2024-05-06 17:35:59 +0300

    login changes

--- controllers/users/userControllers.js
+++ controllers/users/userControllers.js
@@ -3,6 +3,12 @@ const randomstring = require('randomstring'); // Import library randomstring
 const bcrypt = require('bcrypt'); // Import library bcrypt untuk hashing password
 const validator = require('validator'); // Import library validator untuk validasi email
 const transporter = require('../../configs/otp'); // Pastikan Anda memiliki modul transporter yang sesuai dengan kebutuhan Anda
+const jwt = require('jsonwebtoken');
+
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+};
 
 const generateOTP = () => {
     return randomstring.generate({
@@ -64,7 +70,8 @@ const createUser = async (req, res) => {
         
         await transporter.sendMail(mailData);
         
-        res.status(200).json({ user, message: 'Mail Sent' });
+        const token = createToken(user._id);
+        res.status(200).json({ email: user.email, token });
     } catch (error) {
         res.status(400).json({error: error.message})
     }
@@ -94,35 +101,37 @@ const verifyEmail = async (req, res) => {
     }
 };
 
-const loginUser = async (req,res) =>{
-    const {email,password} = req.body;
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({email : email})
-        if(!user){
-            return res.status(404).json({ message: 'User not found' });
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error('User not found');
         }
-        if(!user.isEmailVerified){
-            return res.status(500).json({ message: 'Email need to verify the OTP' });
+        if (!user.isEmailVerified) {
+            throw new Error('Email need to verify the OTP');
         }
-         // Periksa apakah kata sandi yang diberikan cocok dengan yang disimpan dalam catatan pengguna
-         const passwordMatch = await bcrypt.compare(password, user.password);
-         if (!passwordMatch) {
-             return res.status(401).json({ message: 'Incorrect password' });
-         }
- 
-         // Periksa apakah email pengguna sudah diverifikasi
-         if (!user.isEmailVerified) {
-             return res.status(403).json({ message: 'Email not verified' });
-         }
- 
-         // Jika semua verifikasi berhasil, buat token otentikasi
-         const token = jwt.sign({ userId: user._id }, process.env.SECRET, { expiresIn: '1h' });
- 
-         // Kirim token sebagai tanggapan
-         res.status(200).json({ email,token });
+
+        // Periksa apakah kata sandi yang diberikan cocok dengan yang disimpan dalam catatan pengguna
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            throw new Error('Incorrect password');
+        }
+
+        // Periksa apakah email pengguna sudah diverifikasi
+        if (!user.isEmailVerified) {
+            throw new Error('Email not verified');
+        }
+
+        // Jika semua verifikasi berhasil, buat token otentikasi
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET, { expiresIn: '1h' });
+
+        // Kirim token sebagai tanggapan
+        res.status(200).json({ email, token });
     } catch (error) {
-        
+        res.status(500).json({ message: error.message });
     }
 }
+
 module.exports = { createUser ,verifyEmail ,loginUser};
