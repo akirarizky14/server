235d2550d3ead7a5037d15916355b584b166381b
akirarizky14 <akirarizkyyy@gmail.com>
2024-06-01 19:46:19 +0300

    update cat

--- controllers/superadmins/catControllers.js
+++ controllers/superadmins/catControllers.js
@@ -19,33 +19,45 @@ const postCategories = async (req, res) => {
     }
 }
 
-const getAllCategories = async (req,res) =>{
+const getAllCategories = async (req, res) => {
     try {
         const cat = await catModels.find();
-        res.status(200).json(cat)
+        res.status(200).json(cat);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 }
 
-const updateCategories = async (req,res)=>{
+const updateCategories = async (req, res) => {
     try {
-        const id = req.params;
-        const cat = await catModels.findOne({user_id : id});
-        console.log(cat)
+        const { id } = req.params;
+        const { cat_type, name_cat, desc, level_cat } = req.body;
+        const user_id = req.user._id;
 
+        const cat = await catModels.findOneAndUpdate(
+            { _id: id, user_id: user_id },
+            { cat_type, name_cat, desc, level_cat },
+            { new: true } // This option returns the updated document
+        );
+
+        if (!cat) {
+            return res.status(404).json({ error: 'Category not found or not authorized' });
+        }
+
+        res.status(200).json(cat);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 }
 
-const getCategoriesbyUser = async (req,res) =>{
+const getCategoriesbyUser = async (req, res) => {
     try {
-        const user_id = req.params
-        const cat = await catModels.find({user_id})
-        res.status(200).json(cat)
+        const user_id = req.params.user_id;
+        const cat = await catModels.find({ user_id: user_id });
+        res.status(200).json(cat);
     } catch (error) {
-        res.status(400).json({error: error.message});
+        res.status(400).json({ error: error.message });
     }
 }
-module.exports = { postCategories, getAllCategories ,getCategoriesbyUser};
+
+module.exports = { postCategories, updateCategories, getAllCategories, getCategoriesbyUser };
